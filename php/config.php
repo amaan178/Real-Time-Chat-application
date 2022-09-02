a7b18ea880532ba3cf89e6662fc963d97cc2be7c
Amaan Jambura <amaanjambura786@gmail.com>
2022-09-02 21:24:35 +0530

    Made Users page Dynamic and completed Search module with its ajax and validation in backend

--- php/config.php
+++ php/config.php
@@ -5,7 +5,7 @@
     $dbname = "chatapp";
 
     $conn = mysqli_connect($hostname, $username, $password, $dbname);
-    if(!$conn){
-        echo "Database connection error".mysqli_connect_error();
+    if(!$conn) {
+        echo "Database connection error" . mysqli_connect_error();
     }
 ?>
--- users.php
+++ users.php
@@ -1,16 +1,29 @@
+<?php
+    session_start();
+    include_once "php/config.php";
+    if(!isset($_SESSION['unique_id'])){
+        header("location: login.php");
+    }
+?>
 <?php include_once "header.php"; ?>
 <body>
     <div class="wrapper">
         <section class="users">
             <header>
                 <div class="content">
-                    <img src="#" alt="">
+                    <?php
+                    $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+                    if(mysqli_num_rows($sql) > 0){
+                        $row = mysqli_fetch_assoc($sql);
+                    }
+                    ?>
+                    <img src="php/images/<?php echo $row['img']; ?>" alt="">
                     <div class="details">
-                        <span>Amaan Jambura</span>
-                        <p>Active Now</p>
+                        <span><?php echo $row['fname']. " " . $row['lname'] ?></span>
+                        <p><?php echo $row['status']; ?></p>
                     </div>
                 </div>
-                <a href="#!" class="logout">Logout</a>
+                <a href="php/logout.php?logout_id=<?php echo $row['unique_id']; ?>" class="logout">Logout</a>
             </header>
             <div class="search">
                 <span class="text">Select an user to start chat</span>
@@ -22,6 +35,7 @@
             </div>
         </section>
     </div>
-    <script src="javascript/users.js"></script>
+    <script src="javasctipt/users.js"></script>
+
 </body>
 </html>
