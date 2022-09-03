60853d2d6603c79b86fadaafe7915b4f8f1c37c4
Amaan Jambura <amaanjambura786@gmail.com>
2022-09-03 13:02:20 +0530

    Updated code in where added code to show last message on users screen, added function scrollToBottom for the chat, and made active or offline status dynamic of the user

--- javasctipt/chat.js
+++ javasctipt/chat.js
@@ -8,6 +8,23 @@ form.onsubmit = (e)=>{
     e.preventDefault(); // Preventing from Form submitting!
 }
 
+inputField.focus();
+inputField.onkeyup = ()=>{
+    if(inputField.value != ""){
+        sendBtn.classList.add("active");
+    }else{
+        sendBtn.classList.remove("active");
+    }
+}
+
+chatBox.onmouseenter = ()=>{
+    chatBox.classList.add("active");
+}
+
+chatBox.onmouseleave = ()=>{
+    chatBox.classList.remove("active");
+}
+
 sendBtn.onclick = ()=>{
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "php/insert-chat.php", true);
@@ -15,6 +32,7 @@ sendBtn.onclick = ()=>{
         if(xhr.readyState === XMLHttpRequest.DONE){
             if(xhr.status === 200){
                 inputField.value = "";
+                scrollToBottom();
             }
         }
     }
@@ -30,9 +48,16 @@ setInterval(() =>{
             if(xhr.status === 200){
                 let data = xhr.response;
                 chatBox.innerHTML = data;
+                if(!chatBox.classList.contains("active")){
+                    scrollToBottom();
+                }
             }
         }
     }
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.send("incoming_id="+incoming_id);
 }, 500);
+
+function scrollToBottom(){
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
